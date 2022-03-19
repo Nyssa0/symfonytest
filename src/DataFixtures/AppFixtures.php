d1ebb4d7a3cbe0d8d9d387cc8a584df77c88bb9b
« Hassina <hassinaayachi@hotmail.com>
2022-03-19 17:43:13 +0100

    test commit

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,32 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $categories = ['Mac', 'iPad', 'iPhone', 'Watch', 'AirPods', 'TV', 'HomePod', 'Accessories', 'AirTag', 'Polishing Cloth'];
+
+        foreach ($categories as $category) {
+            $categoryEntity = new Category();
+            $categoryEntity->setName($category);
+
+            $manager->persist($categoryEntity);
+
+        }
+
+        $manager->flush();
+
+        $categoryRepo = $manager->getRepository(Category::class);
+        $allCategories = $categoryRepo->findAll();
+
+
+        for ($i = 1; $i <= 200; $i++){
+            $randKey = rand(0, count($allCategories)-1);
+            $product = new Product();
+            $product->setName('Product ' . $i);
+            $product->setPrice(rand(20, 10000));
+            $product->setDescription('Description ' . $i);
+            $product->setStock(rand(0, 10000));
+            $product->setCategory($allCategories[$randKey]);
+            $manager->persist($product);
+        }
 
         $manager->flush();
     }
